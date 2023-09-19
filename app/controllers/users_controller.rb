4509478df1892d199bbcdcc877ac790965954217
mionaga <143914849+mionaga@users.noreply.github.com>
2023-09-19 14:39:31 +0000

    app2-16-user-show-view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def show
+    @user =User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    @user=User.find(params[:id])
   end
 
   def index
+  end
+
+
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :body)
   end
 end
